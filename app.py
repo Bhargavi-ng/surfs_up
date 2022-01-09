43e9feafa613aceeaf3a06b145b8d173ef59d881
Bhargavi-ng <bhargavi.ng@gmail.com>
2022-01-09 00:53:04 -0500

    Created a Flask app and one test route.

---
+++ app.py
@@ -0,0 +1,11 @@
+# Import Flask Dependency
+from flask import Flask
+
+# Create a new Flask app instance
+app = Flask(__name__)
+
+# Create Flask Routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
